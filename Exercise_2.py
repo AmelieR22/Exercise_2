400251b840cbc2b290c7cff17594341ab5afcb9c
AmelieR22 <amelie.robert222@gmail.com>
2026-04-08 23:39:25 +0200

    Problem 1

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,1 +1,24 @@
-Exercise_2
+#Exercise_2
+
+#1
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+
+    def __repr__(self):
+        return f"{self.real} + {self.imag}j"
+
+c1 = Complex(1, 2)
+c2 = Complex(3, 4)
+c3 = c1 + c2
+c4 = c1 - c2
+
+print(c3) # 4 + 6j
+print(c4) # -2 - 2j
